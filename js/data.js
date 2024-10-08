9baae5436978005cd22277f6acc678e0787ba270
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-10-08 21:45:41 +0300

    ex

--- js/data.js
+++ js/data.js
@@ -10,7 +10,7 @@ const products = [
     },
     {
         id:2,
-        img:'../imgs/data/aksiya1.png',
+        img:'../../imgs/data/aksiya1.png',
         title:'Молоко ПРОСТОКВАШИНО паст. питьевое цельное отборное...',
         asli:'44,50',
         dastavka:'50,50',
@@ -19,7 +19,7 @@ const products = [
     },
     {
         id:3,
-        img:'../imgs/data/aksiya6.png',
+        img:'../../imgs/data/aksiya1.png',
         title:'Колбаса сырокопченая МЯСНАЯ ИСТОРИЯ Сальчичон и Тоскан...',
         asli:'44,50',
         dastavka:'50,50',
@@ -28,7 +28,7 @@ const products = [
     },
     {
         id:4,
-        img:'../imgs/data/aksiya5.png',
+        img:'../../imgs/data/aksiya1.png',
         title:'Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...',
         asli:'44,50',
         dastavka:'50,50',
@@ -37,7 +37,7 @@ const products = [
     },
     {
         id:5,
-        img:'../imgs/data/aksiya1.png',
+        img:'../../imgs/data/aksiya1.png',
         title:'Комбайн КЗС-1218 «ДЕСНА-ПОЛЕСЬЕ GS12»',
         asli:'44,50',
         dastavka:'50,50',
@@ -46,7 +46,7 @@ const products = [
     },
     {
         id:6,
-        img:'../imgs/data/aksiya1.png',
+        img:'../../imgs/data/aksiya1.png',
         title:'Колбаса сырокопченая МЯСНАЯ ИСТОРИЯ Сальчичон и Тоскан...',
         asli:'44,50',
         dastavka:'50,50',
@@ -55,7 +55,7 @@ const products = [
     },
     {
         id:7,
-        img:'../imgs/data/aksiya2.png',
+        img:'../../imgs/data/aksiya1.png',
         title:'Комбайн КЗС-1218 «ДЕСНА-ПОЛЕСЬЕ GS12»',
         asli:'44,50',
         dastavka:'50,50',
@@ -64,7 +64,7 @@ const products = [
     },
     {
         id:8,
-        img:'../imgs/data/aksiya3.png',
+        img:'../../imgs/data/aksiya1.png',
         title:'Комбайн КЗС-1218 «ДЕСНА-ПОЛЕСЬЕ GS12»',
         asli:'44,50',
         dastavka:'50,50',
@@ -73,7 +73,7 @@ const products = [
     },
     {
         id:9,
-        img:'../imgs/data/aksiya1.png',
+        img:'../../imgs/data/aksiya1.png',
         title:'Комбайн КЗС-1218 «ДЕСНА-ПОЛЕСЬЕ GS12»',
         asli:'44,50',
         dastavka:'50,50',
@@ -82,7 +82,7 @@ const products = [
     },
     {
         id:10,
-        img:'../imgs/data/aksiya1.png',
+        img:'../../imgs/data/aksiya1.png',
         title:'Г/Ц Блинчики с мясом вес, Россия',
         asli:'44,50',
         dastavka:'50,50',
