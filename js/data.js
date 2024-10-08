6c954222502386d8a71f3f13605c1decea5fd662
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-10-08 19:28:05 +0300

    add data

--- js/data.js
+++ js/data.js
@@ -1,5 +1,93 @@
 const aksiya = [
     {
         id:1,
-    }
+        img:'../imgs/data/aksiya1.png',
+        title:'Г/Ц Блинчики с мясом вес, Россия',
+        asli:'44,50',
+        dastavka:'50,50',
+        aksiya:'-50%',
+        yulduz:'⭐⭐⭐⭐⭐'
+    },
+    {
+        id:2,
+        img:'../imgs/data/aksiya1.png',
+        title:'Молоко ПРОСТОКВАШИНО паст. питьевое цельное отборное...',
+        asli:'44,50',
+        dastavka:'50,50',
+        aksiya:'-50%',
+        yulduz:'⭐⭐⭐⭐⭐'
+    },
+    {
+        id:3,
+        img:'../imgs/data/aksiya1.png',
+        title:'Колбаса сырокопченая МЯСНАЯ ИСТОРИЯ Сальчичон и Тоскан...',
+        asli:'44,50',
+        dastavka:'50,50',
+        aksiya:'-50%',
+        yulduz:'⭐⭐⭐⭐⭐'
+    },
+    {
+        id:4,
+        img:'../imgs/data/aksiya1.png',
+        title:'Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...',
+        asli:'44,50',
+        dastavka:'50,50',
+        aksiya:'-50%',
+        yulduz:'⭐⭐⭐⭐⭐'
+    },
+    {
+        id:5,
+        img:'../imgs/data/aksiya1.png',
+        title:'Комбайн КЗС-1218 «ДЕСНА-ПОЛЕСЬЕ GS12»',
+        asli:'44,50',
+        dastavka:'50,50',
+        aksiya:'-50%',
+        yulduz:'⭐⭐⭐⭐⭐'
+    },
+    {
+        id:6,
+        img:'../imgs/data/aksiya1.png',
+        title:'Колбаса сырокопченая МЯСНАЯ ИСТОРИЯ Сальчичон и Тоскан...',
+        asli:'44,50',
+        dastavka:'50,50',
+        aksiya:'-50%',
+        yulduz:'⭐⭐⭐⭐⭐'
+    },
+    {
+        id:7,
+        img:'../imgs/data/aksiya1.png',
+        title:'Комбайн КЗС-1218 «ДЕСНА-ПОЛЕСЬЕ GS12»',
+        asli:'44,50',
+        dastavka:'50,50',
+        aksiya:'-50%',
+        yulduz:'⭐⭐⭐⭐⭐'
+    },
+    {
+        id:8,
+        img:'../imgs/data/aksiya1.png',
+        title:'Комбайн КЗС-1218 «ДЕСНА-ПОЛЕСЬЕ GS12»',
+        asli:'44,50',
+        dastavka:'50,50',
+        aksiya:'-50%',
+        yulduz:'⭐⭐⭐⭐⭐'
+    },
+    {
+        id:9,
+        img:'../imgs/data/aksiya1.png',
+        title:'Комбайн КЗС-1218 «ДЕСНА-ПОЛЕСЬЕ GS12»',
+        asli:'44,50',
+        dastavka:'50,50',
+        aksiya:'-50%',
+        yulduz:'⭐⭐⭐⭐⭐'
+    },
+    {
+        id:10,
+        img:'../imgs/data/aksiya1.png',
+        title:'Г/Ц Блинчики с мясом вес, Россия',
+        asli:'44,50',
+        dastavka:'50,50',
+        aksiya:'-50%',
+        yulduz:'⭐⭐⭐⭐⭐'
+    },
+
 ]
