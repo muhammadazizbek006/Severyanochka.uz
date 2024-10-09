2220e1e3901f6128f415155f2835c843252cf4a4
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-10-09 11:20:12 +0300

    add carts aksiya

--- js/app.js
+++ js/app.js
@@ -6,16 +6,31 @@ const productsContainer = document.getElementById('products-container');
 products.forEach((product) => {
   // Mahsulot kartochkasi HTML yaratish
   const productCard = `
-    <div class="product-card">
-      <img src="${product.image}" alt="${product.name}">
+    <li class="product-card">
+    <button class="product-like-btn">
+      <img class="product-like-img" src="../imgs/like.svg" alt="like">
+    </button>
+    <div>
+       <img src="${product.img}" alt="${product.title}">
+    </div>
       <div class="product-info">
-        <h3>${product.name}</h3>
-        <p>${product.price}</p>
-        <span>${product.discount}</span>
-        <div class="rating">${'★'.repeat(Math.floor(product.rating))}</div>
+
+      <div class='narx-wrapper'>
+      <div>
+      <p class="aslNarx">${product.asli} ₽</p>
+      <span class='spen'>С картой</span>
+      </div>
+          <div>
+            <p class="dastavkaNarx">${product.dastavka} ₽ </p>
+              <span class='spen'>Обычная</span>
+          </div>
+        
+      </div>
+      <h3 class='product-title'>${product.title}</h3>
+      <div class="rating">${'★'.repeat(Math.floor(product.rating))}</div>
         <button class="add-to-cart">В корзину</button>
       </div>
-    </div>
+    </li>
   `;
 
   // Mahsulotni sahifaga qo'shish
